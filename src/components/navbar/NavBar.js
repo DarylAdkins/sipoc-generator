fcb1c8901b56f168a765af88882dac21a14c76ed
Daryl Adkins <daryl.adkins@gmail.com>
2019-10-17 14:47:14 -0400

    navbar with css

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,48 +1,55 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom"
-// import './NavBar.css'
+import './NavBar.css'
 import auth0Client from "../auth/Auth";
 
 class NavBar extends Component {
- signOut = () => {
-    auth0Client.signOut();
-    sessionStorage.clear()
-    this.props.history.replace("/");
-  };
+    signOut = () => {
+        auth0Client.signOut();
+        sessionStorage.clear()
+        this.props.history.replace("/");
+    };
 
-  render(){
+    render() {
 
-    return (
-      <header>
-        <h1 className="site-title">SIPOC Generator<br />
+        return (
+            <header>
+                <h1 className="site-title">SIPOC Generator<br />
 
-        </h1>
-        <nav>
-          <ul className="container">
-            <li><Link className="nav-link" to="/home">Home</Link></li>
-            {!auth0Client.isAuthenticated() ? (
-              <button className="btn btn-success" onClick={auth0Client.signIn}>Sign In</button>
-        ) : (
-            <React.Fragment>
-             <label>
-                {auth0Client.getProfile().name}
-              </label>
-              <button
-                className="btn btn-danger"
-                onClick={this.signOut}
-              >
-                Sign Out
+                </h1>
+                <nav>
+
+                    {!auth0Client.isAuthenticated() ? (
+                        <p id="signinbuttonposition"><button id="signinbutton" className="btn btn-success" onClick={auth0Client.signIn}>Please Sign In</button></p>
+                    ) : (
+                            <React.Fragment>
+                                <div className="container">
+                                    <p><Link className="nav-link" to="/home">Home</Link></p>
+                                    <p><Link className="nav-link" to="/sipoc">  View/Create/Edit SIPOCS  </Link></p>
+                                    <p><Link className="nav-link" to="/supplier/new">  Enter New Supplier  </Link></p>
+
+                                    <div id="userdiv">
+                                        <div id="user">
+                                            {auth0Client.getProfile().name}
+                                            </div>
+                                    </div>
+
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={this.signOut}
+                                    >
+                                        Sign Out
               </button>
-             <Link className="nav-link" to="/sipoc">  View/Create/Edit SIPOCS  </Link>
-             <Link className="nav-link" to="/supplier/new">  Enter New Supplier  </Link>
+                                </div>
 
-             </React.Fragment>
-            )}
-          </ul>
-        </nav>
-      </header>
-    )
-  }
+
+                            </React.Fragment>
+                        )}
+
+                </nav>
+            </header>
+        )
+    }
 }
 
 export default withRouter(NavBar);
